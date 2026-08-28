88db9d8619c03d583fb434cd8dbaed558954c765
alexandre14k <26510885+alexandre14k@users.noreply.github.com>
2026-08-28 22:32:05 +0200

    update ext stm32

--- ext/ext.py
+++ ext/ext.py
@@ -5,6 +5,47 @@ import shutil
 import re
 
 
+def ext_dir():
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+def ext_items():
+    return [
+        (
+            "stm32", "hal",
+            "https://github.com/STMicroelectronics/"
+            "stm32f4xx-hal-driver",
+            None
+        ),
+        (
+            "stm32", "cmsis_device",
+            "https://github.com/STMicroelectronics/"
+            "cmsis_device_f4",
+            None
+        ),
+        (
+            "stm32", "cmsis_core",
+            "https://github.com/ARM-software/CMSIS_5",
+            ["CMSIS/Core/Include"]
+        ),
+    ]
+
+
+def ext_item_dir(group, name):
+    return os.path.join(ext_dir(), group, name)
+
+
+def ext_item_relpath(group, name):
+    return os.path.relpath(
+        ext_item_dir(group, name),
+        os.path.dirname(ext_dir())
+    )
+
+
+def ext_clone_relpath(path):
+    return os.path.relpath(path, ext_dir())
+
+
 def ext_commit_short(path):
     process = subprocess.run(
         ["git", "rev-parse", "--short", "HEAD"],
@@ -18,43 +59,9 @@ def ext_commit_short(path):
     return commit
 
 
-def ext_archive_name(name, path):
-    return (
-        "stm32_" + ext_arch() + "_" + name +
-        "_" + ext_commit_short(path)
-    )
-
-def ext_archive_one(name):
-    path = ext_repo_dir(name)
-    if not os.path.isdir(path):
-        print(name + " not cloned, skip")
-        return 0
-    base = ext_archive_name(name, path)
-    dest = os.path.join(ext_dir(), base)
-    zip_path = dest + ".zip"
-    if os.path.isfile(zip_path):
-        os.remove(zip_path)
-    shutil.make_archive(dest, "zip", path)
-    print("archived -- " + base + ".zip")
-    return 0
-
-
-def ext_archive():
-    code = 0
-    for name, _ in ext_repos():
-        result = ext_archive_one(name)
-        if result != 0:
-            code = result
-    return code
-
-
-def ext_dir():
-    return os.path.dirname(os.path.abspath(__file__))
-
-
 def ext_hal_url():
-    for name, url in ext_repos():
-        if name == "hal":
+    for group, name, url, _ in ext_items():
+        if group == "stm32" and name == "hal":
             return url
     return ""
 
@@ -64,45 +71,6 @@ def ext_arch():
     if match:
         return match.group(1)
     return "unknown"
-
-
-def ext_repos():
-    return [
-        (
-            "hal",
-            "https://github.com/STMicroelectronics/"
-            "stm32f4xx-hal-driver"
-        ),
-        (
-            "cmsis_device",
-            "https://github.com/STMicroelectronics/"
-            "cmsis_device_f4"
-        ),
-        (
-            "cmsis_core",
-            "https://github.com/ARM-software/CMSIS_5"
-        ),
-    ]
-
-def ext_sparse_paths():
-    return {
-        "cmsis_core": ["CMSIS/Core/Include"],
-    }
-
-
-def ext_repo_dir(name):
-    return os.path.join(ext_dir(), "stm32", name)
-
-
-def ext_clone_relpath(path):
-    return os.path.relpath(path, ext_dir())
-
-
-def ext_relpath(name):
-    return os.path.relpath(
-        ext_repo_dir(name),
-        os.path.dirname(ext_dir())
-    )
 
 
 def ext_run(cmd, cwd=None):
@@ -159,9 +127,9 @@ def ext_clone_full(url, path):
     return ext_run(["git", "clone", url, ext_clone_relpath(path)])
 
 
-def ext_update_one(name, url):
-    path = ext_repo_dir(name)
-    print('path is "' + ext_relpath(name) + '"')
+def ext_update_one(group, name, url, paths):
+    path = ext_item_dir(group, name)
+    print('path is "' + ext_item_relpath(group, name) + '"')
     if os.path.isdir(path) and ext_has_origin(path):
         ext_run(["git", "fetch", "origin"], path)
         branch = ext_default_branch(path)
@@ -169,7 +137,6 @@ def ext_update_one(name, url):
             ["git", "reset", "--hard", "origin/" + branch],
             path
         )
-    paths = ext_sparse_paths().get(name)
     if paths:
         return ext_clone_sparse(url, path, paths)
     return ext_clone_full(url, path)
@@ -177,8 +144,39 @@ def ext_update_one(name, url):
 
 def ext_update():
     code = 0
-    for name, url in ext_repos():
-        result = ext_update_one(name, url)
+    for group, name, url, paths in ext_items():
+        result = ext_update_one(group, name, url, paths)
+        if result != 0:
+            code = result
+    return code
+
+
+def ext_archive_prefix(group, name):
+    return "stm32_" + ext_arch() + "_" + name
+
+
+def ext_archive_one(group, name):
+    path = ext_item_dir(group, name)
+    if not os.path.isdir(path):
+        print(name + " not cloned, skip")
+        return 0
+    base = (
+        ext_archive_prefix(group, name) +
+        "_" + ext_commit_short(path)
+    )
+    dest = os.path.join(ext_dir(), base)
+    zip_path = dest + ".zip"
+    if os.path.isfile(zip_path):
+        os.remove(zip_path)
+    shutil.make_archive(dest, "zip", path)
+    print("archived -- " + base + ".zip")
+    return 0
+
+
+def ext_archive():
+    code = 0
+    for group, name, _, _ in ext_items():
+        result = ext_archive_one(group, name)
         if result != 0:
             code = result
     return code
@@ -189,8 +187,8 @@ def ext_confirm(prompt):
     return answer == "y"
 
 
-def ext_remove_one(name):
-    path = ext_repo_dir(name)
+def ext_remove_one(group, name):
+    path = ext_item_dir(group, name)
     if not os.path.isdir(path):
         return 0
     if sys.platform.startswith("win"):
@@ -199,15 +197,18 @@ def ext_remove_one(name):
 
 
 def ext_clean():
-    names = [name for name, _ in ext_repos()
-             if os.path.isdir(ext_repo_dir(name))]
-    if not names:
+    present = [
+        (group, name) for group, name, _, _ in ext_items()
+        if os.path.isdir(ext_item_dir(group, name))
+    ]
+    if not present:
         return 0
-    if not ext_confirm("remove " + ", ".join(names) + " ?"):
+    labels = [name for _, name in present]
+    if not ext_confirm("remove " + ", ".join(labels) + " ?"):
         return 1
     code = 0
-    for name in names:
-        result = ext_remove_one(name)
+    for group, name in present:
+        result = ext_remove_one(group, name)
         if result != 0:
             code = result
     return code
@@ -215,9 +216,9 @@ def ext_clean():
 
 def ext_menu():
     print("# arch " + ext_arch())
-    print("u -- update stm32 folder")
-    print("c -- clean stm32 folder")
-    print("a -- archive stm32 folder")
+    print("u -- update ext folder")
+    print("c -- clean ext folder")
+    print("a -- archive ext folder")
     print("m -- show the menu")
     print("x -- exit")
 
